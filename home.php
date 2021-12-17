76ba0bcc9c0c7107213734c70edc22fe133b7a55
URIAN VIERA <urian1213viera@gmail.com>
2021-12-17 13:56:06 -0600

    Update home.php

--- home.php
+++ home.php
@@ -18,6 +18,8 @@ if (isset($_SESSION['emailUser']) != "") {
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/perfil.css">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/perfil.jpeg">
+      
     <title>Inicio :: <?php echo $nameUser; ?></title>
   </head>
   <body>
